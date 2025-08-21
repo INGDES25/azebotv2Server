86a6693858c34195619f34d532b6d237d009a878
INGDES25 <sagedessaints@gmail.com>
2025-08-21 11:37:33 +0100

    Modification  des codes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],   
+  allowedHeaders: ['Content-Type', 'Authorization'],     
   credentials: true  
 }));
 
